092fea69a5354627d5576fee04a343d3e0410494
sabgarian <stefan.abgarian@gmail.com>
2024-04-14 20:02:39 -0700

    lots of stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
-    Vector2 direction;
-    // Start is called before the first frame update
-    void Start()
-    {
-        direction = Vector2.zero;
-        rb = gameObject.GetComponent<Rigidbody>();
-    }
+    public GameObject waterCreature;
 
-    // Update is called once per frame
+    public GameOverScreen gameOverScreen;
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float jumpPower = 16f;
+    private bool isFacingRight = true;
+    private bool hasActiveCreature = false;
+    
     void Update()
     {
-        
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            summonCreature();
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.Setup();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.gameObject.name == "Hazards")
+        //{
+        //    GameOver();
+        //    gameObject.SetActive(false);
+        //}
+    }
+
+    private void summonCreature()
+    {
+        if (!hasActiveCreature)
+        {
+            waterCreature.SetActive(true);
+            hasActiveCreature = true;
+        }
+        else if (hasActiveCreature)
+        {
+            waterCreature.SetActive(false);
+            hasActiveCreature = false;
+        }
     }
 }
---
+++ Assets/Scripts/WaterCreature.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WaterCreature : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Hazards")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
